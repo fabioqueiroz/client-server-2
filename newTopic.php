8ef0072db98a0bfcc23ebf32adba630ec136c0be
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-11-18 21:58:33 +0000

    updated topic creation

--- newTopic.php
+++ newTopic.php
@@ -10,10 +10,12 @@ $postingUser = $_SESSION['userID'];
 if(isset($_POST['subject']) && isset($_POST['category']) && isset($_POST['description'])) {
     $categoryID = $topicDataSet->getCategoryID($_POST['category']);
     $result = $topicDataSet->createTopic($_POST['subject'], $categoryID, $postingUser, $_POST['description']);
+
+    $view->isTopicCreated = true;
 }
 
 // uncomment to stop using modal
-//require_once('Views/newTopic.phtml');
+require_once('Views/newTopic.phtml');
 
 // comment out if not using modal
-require_once('forum.php');
+//require_once('forum.php');
