68b1eaf6f05082c95ec95d8dea39bdf9b8d7d84f
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-11-26 19:25:42 +0000

    enable deletion from checkboxes

--- watchList.php
+++ watchList.php
@@ -8,10 +8,17 @@ $watchlistDataSet = new WatchlistDataSet();
 $subscribedPosts = $watchlistDataSet->getSubscriptions($_SESSION['userID']);
 //var_dump($subscribedPosts);
 
-echo $_POST['getWatchlistID'];
+$ids = $_POST['id'];
+//var_dump($ids);
 
-//if(isset($_POST['remove-from-watchlist']) && $_POST['remove-rand-check'] == $_SESSION['unsubscribe-rand']) {
-//
-//}
+if (isset($_POST['id'])) {
+    foreach($ids as $id)
+    {
+        if(isset($_POST['remove-from-watchlist']) && $_POST['remove-rand-check'] == $_SESSION['unsubscribe-rand']) {
+            $watchlistDataSet->removeFromWatchlist($id);
+            $subscribedPosts = $watchlistDataSet->getSubscriptions($_SESSION['userID']);
+        }
+    }
+}
 
 require_once('Views/watchList.phtml');
