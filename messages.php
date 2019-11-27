b83a6ae2a14736d4527a4ed8bfc3de5ef53c9dcd
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-11-27 00:11:43 +0000

    messages controller

---
+++ messages.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require_once('Models/Mailbox/MailboxDataSet.php');
+require_once('Models/Messages/MessageDataSet.php');
+
+$view = new stdClass();
+$mailboxDataSet = new MailboxDataSet();
+$messageDataSet = new MessageDataSet();
+
+
+
+require_once('Views/messages.phtml');
