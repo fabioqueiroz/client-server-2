459a8289a8577a4445c6357378862044ae113c46
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-11-28 16:07:49 +0000

    outbox complete

--- outbox.php
+++ outbox.php
@@ -5,8 +5,22 @@ require_once('Models/Messages/MessageDataSet.php');
 
 $view = new stdClass();
 $messageDataSet = new MessageDataSet();
+$mailboxDataSet = new MailboxDataSet();
 
 $outbox = $messageDataSet->getOutboxMail($_SESSION['userID']);
-var_dump($outbox);
+//var_dump($outbox);
 
+$ids = $_POST['id'];
+//var_dump($ids);
+
+if (isset($_POST['id'])) {
+    foreach($ids as $id)
+    {
+        if(isset($_POST['delete-from-inbox']) && $_POST['delete-rand-check'] == $_SESSION['delete-rand']) {
+
+            $mailboxDataSet->deleteFromMailboxById($id);
+            $outbox = $messageDataSet->getOutboxMail($_SESSION['userID']);
+        }
+    }
+}
 require_once('Views/outbox.phtml');
