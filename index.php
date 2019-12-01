1c94dd3dab34eeaf201b37234ab31be092bd52cf
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-12-01 22:13:10 +0000

    home new layout

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
+require_once('Models/Categories/CategoryDataSet.php');
+require_once('Models/Topics/TopicDataSet.php');
 
 $view = new stdClass();
-$view->pageTitle = 'Homepage';
+$categoryDataSet = new CategoryDataSet();
+$topicDataSet = new TopicDataSet();
+
 require_once('Views/index.phtml');
 
 
