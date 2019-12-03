7fe36d0b35d9156b809deea2b3e94650fa5ae54a
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-12-03 18:09:43 +0000

    created user info page

--- profile.php
+++ profile.php
@@ -1,4 +1,11 @@
 <?php
 session_start();
+require_once('Models/Users/UserDataSet.php');
+
+$view = new stdClass();
+$userDataSet = new UserDataSet();
+
+$user = $userDataSet->getUserById($_SESSION['userID']);
+//var_dump($user);
 
 require_once('Views/profile.phtml');
