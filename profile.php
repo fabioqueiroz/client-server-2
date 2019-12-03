77601c0cde2a6937c3512e06afbd937e13f065ac
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-12-03 20:41:36 +0000

    corrected logic

--- profile.php
+++ profile.php
@@ -13,28 +13,33 @@ if(strlen($_POST['firstName']) < 0 || strlen($_POST['firstName']) > 45 || strlen
 
 $hashedPwdInDb = $userDataSet->passwordChecker($_SESSION['email']);
 
-if(password_verify($_POST['password'], $hashedPwdInDb)) {
+if(isset($_POST['password']) && !empty(isset($_POST['password']))) {
 
-    if(isset($_POST['firstName']) && !empty($_POST['firstName'])) {
-        if(isset($_POST['firstName']) && $_POST['rand-check'] == $_SESSION['rand']) {
-            $firstName = strip_tags(trim(($_POST['firstName'])));
-            $userDataSet->updateFirstName($firstName, $_SESSION['userID']);
+    if(password_verify($_POST['password'], $hashedPwdInDb)) {
+
+        if(isset($_POST['firstName']) && !empty($_POST['firstName'])) {
+            if(isset($_POST['firstName']) && $_POST['rand-check'] == $_SESSION['rand']) {
+                $firstName = strip_tags(trim(($_POST['firstName'])));
+                $userDataSet->updateFirstName($firstName, $_SESSION['userID']);
+            }
+        }
+
+        if(isset($_POST['lastName']) && !empty($_POST['lastName'])) {
+            if(isset($_POST['lastName']) && $_POST['rand-check'] == $_SESSION['rand']) {
+                $lastName = strip_tags(trim(($_POST['lastName'])));
+                $userDataSet->updateLastName($lastName, $_SESSION['userID']);
+            }
         }
     }
 
-    if(isset($_POST['lastName']) && !empty($_POST['lastName'])) {
-        if(isset($_POST['lastName']) && $_POST['rand-check'] == $_SESSION['rand']) {
-            $lastName = strip_tags(trim(($_POST['lastName'])));
-            $userDataSet->updateLastName($lastName, $_SESSION['userID']);
-        }
+    else {
+        $view->loginError = true;
     }
 
     $user = $userDataSet->getUserById($_SESSION['userID']);
 }
 
-else {
-    $view->loginError = true;
-}
+
 
 
 
